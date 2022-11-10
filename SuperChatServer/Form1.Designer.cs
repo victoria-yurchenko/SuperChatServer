4333594923fe24b8ef167e73ea646f1bacf57f69
ACER <ACER@DESKTOP-GDVFM0R>
2022-11-10 14:29:57 +0200

    Add project files.

---
+++ SuperChatServer/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SuperChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbUsers = new System.Windows.Forms.ListBox();
+            this.btListen = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbMessages = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbUsers);
+            this.groupBox1.Controls.Add(this.btListen);
+            this.groupBox1.Controls.Add(this.tbPort);
+            this.groupBox1.Controls.Add(this.tbAddress);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(321, 532);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chat State";
+            // 
+            // lbUsers
+            // 
+            this.lbUsers.FormattingEnabled = true;
+            this.lbUsers.ItemHeight = 20;
+            this.lbUsers.Location = new System.Drawing.Point(9, 81);
+            this.lbUsers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lbUsers.Name = "lbUsers";
+            this.lbUsers.ScrollAlwaysVisible = true;
+            this.lbUsers.Size = new System.Drawing.Size(305, 384);
+            this.lbUsers.TabIndex = 3;
+            // 
+            // btListen
+            // 
+            this.btListen.Location = new System.Drawing.Point(195, 21);
+            this.btListen.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btListen.Name = "btListen";
+            this.btListen.Size = new System.Drawing.Size(119, 31);
+            this.btListen.TabIndex = 2;
+            this.btListen.Text = "Listen";
+            this.btListen.UseVisualStyleBackColor = true;
+            this.btListen.Click += new System.EventHandler(this.btListen_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(114, 23);
+            this.tbPort.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(74, 27);
+            this.tbPort.TabIndex = 1;
+            this.tbPort.Text = "8888";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(9, 23);
+            this.tbAddress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(98, 27);
+            this.tbAddress.TabIndex = 0;
+            this.tbAddress.Text = "127.0.0.1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbMessages);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(328, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(442, 532);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Messages";
+            // 
+            // tbMessages
+            // 
+            this.tbMessages.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbMessages.Location = new System.Drawing.Point(3, 24);
+            this.tbMessages.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbMessages.Multiline = true;
+            this.tbMessages.Name = "tbMessages";
+            this.tbMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMessages.Size = new System.Drawing.Size(436, 440);
+            this.tbMessages.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 532);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btListen;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbUsers;
+        private System.Windows.Forms.TextBox tbMessages;
+    }
+}
---
+++ SuperChatServer/Form1.cs
@@ -0,0 +1,138 @@
+﻿using ChatDataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SuperChatServer
+{
+    public partial class Form1 : Form
+    {
+        TcpListener server = null;
+        Semaphore semaphore = null;
+        static List<string> users = new List<string>();
+        static Dictionary<string, List<ChatData>> messages = new Dictionary<string, List<ChatData>>();
+        BinaryFormatter formatter = null;
+        public Form1()
+        {
+            InitializeComponent();
+            semaphore = new Semaphore(10, 10);
+            formatter = new BinaryFormatter();
+        }
+
+        private void btListen_Click(object sender, EventArgs e)
+        {
+            string address = tbAddress.Text.Trim();
+            if( string.IsNullOrEmpty( address ))
+            {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach(var item in host.AddressList)
+                {
+                    if(item.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        address = item.ToString();
+                        tbAddress.Text = address;
+                        break;
+                    }
+                }
+            }
+            server = new TcpListener(IPAddress.Parse(address),
+                Convert.ToInt32(tbPort.Text.Trim()));
+            server.Start();
+            btListen.BackColor = Color.GreenYellow;
+            btListen.Text = "Listening...";
+
+            Thread thread = new Thread(WaitForClientConnect);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void WaitForClientConnect()
+        {
+            while(true)
+            {
+                semaphore.WaitOne();
+                Thread thread = new Thread(Dialog);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        private void Dialog(object obj)
+        {
+            TcpClient client = server.AcceptTcpClient();            //app blocks here
+            NetworkStream ns = client.GetStream();
+            while(true)
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(ns, Encoding.UTF8);
+                    ChatData chatData = (ChatData)formatter.Deserialize(sr.BaseStream);
+                    switch(chatData.Command)
+                    {
+                        case "Register":
+                            break;
+                        case "Login":
+                            string name = users.Find(n => chatData.FromName == n);
+                            if(string.IsNullOrEmpty(name))
+                            {
+                                users.Add(chatData.FromName);
+                                messages.Add(chatData.FromName, new List<ChatData>());
+                                lbUsers.Items.Add(chatData.FromName);
+                            }
+                            else
+                            {
+                                //TO DO
+                            }
+                            break;
+                        case "Text":
+                            if(string.IsNullOrEmpty(chatData.ToName))
+                            {
+                                string info = "\r\n" + chatData.FromName + ">>" + chatData.Text +
+                                                                "\t" + DateTime.Now.ToShortTimeString();
+                                this.Invoke((MethodInvoker)delegate ()
+                                {
+                                    tbMessages.Text += info;
+                                });
+
+                                foreach (KeyValuePair<string, List<ChatData>> item in messages)
+                                {
+                                    item.Value.Add(chatData);
+                                }
+                            }
+                            else
+                            {
+                                //process private message - TO DO
+                            }
+                            
+                            break;
+                        case "Data":
+                            break;
+                        case "Sync":
+
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void DownloadData(byte[] data, string fileName)
+        {
+            using (var binaryWriter = new BinaryWriter(File.OpenWrite(fileName)))
+            {
+                binaryWriter.Write(data);
+            }
+        }
+    }
+}
